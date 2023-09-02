894f9d80002b01440b7d1da209a7b1502a0cf35b
vitor <vitorsalves0812@gmail.com>
2023-09-01 22:48:01 -0300

    criada view de cadastro de vagas

---
+++ CadastraVagas/CadastraVagas/Controllers/CadastraVagaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastraVagas.Controllers
+{
+    public class CadastraVagaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CadastraVagas/CadastraVagas/Controllers/ListaVagasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastraVagas.Controllers
+{
+    public class ListaVagasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
