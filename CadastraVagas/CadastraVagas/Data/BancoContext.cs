e2b91a7f35141aa095215e904c7f368b1759e4e0
vitor <vitorsalves0812@gmail.com>
2023-09-06 21:48:33 -0300

    Criado o contexto de banco de dados

---
+++ CadastraVagas/CadastraVagas/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using CadastraVagas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastraVagas.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<VagaModel> Vagas { get; set; }
+    }
+}
---
+++ CadastraVagas/CadastraVagas/Models/VagaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastraVagas.Models
+{
+    public class VagaModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Empresa { get; set; }
+        public string Descricao { get; set; }
+        public string Link { get; set; }
+        public bool Aprovado { get; set; }
+    }
+}
